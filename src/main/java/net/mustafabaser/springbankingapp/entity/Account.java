8a984d135da5cb574474d2b4086230374c0187b3
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-04-23 23:50:12 +0300

    Entity created

---
+++ src/main/java/net/mustafabaser/springbankingapp/entity/Account.java
@@ -0,0 +1,25 @@
+package net.mustafabaser.springbankingapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="accounts")
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="account_holder_name")
+    private String accountHolderName;
+
+    private double balance;
+}
